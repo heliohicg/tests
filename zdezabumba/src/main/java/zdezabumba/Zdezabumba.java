d8666b5c6ccd9f48b9006596e121491549dfce1f
Ibraim <hicg1909@gmail.com>
2024-06-15 18:16:53 -0300

    Z DE ZABUMBA FOREVER!!!

---
+++ zdezabumba/src/main/java/zdezabumba/Zdezabumba.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package zdezabumba;
+
+/**
+ *
+ * @author user
+ */
+public class Zdezabumba {
+
+    public static void main(String[] args) {
+        System.out.println("Z DE ZABUMBA FOREVER!!!");
+        System.out.println("Z DE ZABUMBA FOREVER!!!");
+        System.out.println("Z DE ZABUMBA FOREVER!!!");
+        System.out.println("Z DE ZABUMBA FOREVER!!!");
+        System.out.println("Z DE ZABUMBA FOREVER!!!");
+    }
+}
