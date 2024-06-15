10c7fd6e25332d8f272b73952b818110e7dd99e7
Ibraim <hicg1909@gmail.com>
2024-06-15 18:32:19 -0300

    teste GITHUB

---
+++ src/githubtests/Tests.java
@@ -0,0 +1,28 @@
+package githubtests;
+
+public class Tests {
+	 public static void main(String[] args) {
+		 int x =0;
+		System.out.println("Z DE ZABUMBA FOREVER ! ! !");
+		x++;
+		System.out.println("Z DE ZABUMBA FOREVER ! ! !");
+		x++;
+		System.out.println("Z DE ZABUMBA FOREVER ! ! !");
+		x++;
+		System.out.println("Z DE ZABUMBA FOREVER ! ! !");
+		x++;
+		System.out.println("Z DE ZABUMBA FOREVER ! ! !");
+		x++;
+		System.out.println("Z DE ZABUMBA FOREVER ! ! !");
+		x++;
+		System.out.println("Z DE ZABUMBA FOREVER ! ! !");
+		x++;
+		x++;
+		System.out.println(x);
+		
+		
+		
+		
+	}
+
+}
